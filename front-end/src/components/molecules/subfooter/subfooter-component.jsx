e880ba44b3a924a10a2db970dbd256c44f7c7554
G.Cristian <golu.cristian22@gmail.com>
2023-04-20 12:01:14 +0300

    feat(front-end): added Subfooter Component

---
+++ front-end/src/components/molecules/subfooter/subfooter-component.jsx
@@ -0,0 +1,32 @@
+import "./subfooter-styles.scss";
+import Icon from "../../atoms/icon/icon-component";
+
+function Subfooter() {
+  return (
+    <div className="subfooter">
+      <div className="subfooter__content">
+        <p className="content__copyright text-size-lg">©2023 Golu Cristian</p>
+        <div className="content__items">
+          <a className="content__link" href="/">
+            <Icon
+              iconName="github"
+              iconWidth="28"
+              iconHeight="28"
+              iconColor="var(--color-accent-primary)"
+            />
+          </a>
+          <a className="content__link" href="/">
+            <Icon
+              iconName="linkedin"
+              iconWidth="28"
+              iconHeight="28"
+              iconColor="var(--color-accent-primary)"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Subfooter;
