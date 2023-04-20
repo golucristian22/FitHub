9509bdbf18e1c6e0fe2f27664e4a74681d7d7eb8
G.Cristian <golu.cristian22@gmail.com>
2023-04-20 17:03:18 +0300

    feat(front-end): added Menu Component

---
+++ front-end/src/components/atoms/menu/menu-component.jsx
@@ -0,0 +1,27 @@
+import "./menu-styles.scss";
+
+function Menu() {
+  return (
+    <div className="menu" onClick={onMenuClick}>
+      <div className="menu__line menu__line--first"></div>
+      <div className="menu__line menu__line--second"></div>
+      <div className="menu__line menu__line--third"></div>
+    </div>
+  );
+}
+
+function menuEventDispatcher() {
+  const event = new CustomEvent("onMenuClick", {
+    bubbles: true,
+    composed: true,
+  });
+  document.dispatchEvent(event);
+}
+
+function onMenuClick() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("menu--open");
+  menuEventDispatcher();
+}
+
+export default Menu;
