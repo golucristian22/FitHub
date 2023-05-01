9b0da7cc572ec51e95b0e13a0c7d091adb25d9a0
G.Cristian <golu.cristian22@gmail.com>
2023-05-01 13:18:39 +0300

    fix(front-end/banner): added alt attribute to image

--- front-end/src/components/organisms/banner/banner-component.tsx
+++ front-end/src/components/organisms/banner/banner-component.tsx
@@ -5,7 +5,7 @@ import BannerImage from "../../../assets/images/banner.png";
 function Banner() {
   return (
     <section className="banner" style={ {paddingBottom: ""} }>
-      <img className="banner__image" src={BannerImage} />
+      <img className="banner__image" src={BannerImage} alt="Placeholder" />
       <div className="banner__text-container">
         <h3 className="banner__text">
           Power Up Your Performance with FitHub Supplements
