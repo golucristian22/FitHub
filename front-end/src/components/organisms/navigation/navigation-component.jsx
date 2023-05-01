698004e0bf965a87d548f45a0217789bdd72065a
G.Cristian <golu.cristian22@gmail.com>
2023-05-01 11:49:17 +0300

    fix(front-end/navigation): now it displays the correct navigation on every device

--- front-end/src/components/organisms/navigation/navigation-component.jsx
+++ front-end/src/components/organisms/navigation/navigation-component.jsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
 import NavDesktop from "./navigation-desktop/navigation-desktop-component";
 import NavMobile from "./navigation-mobile/navigation-mobile-component";
+
 function Nav() {
   const [isMatchingMedia, setIsMatchingMedia] = useState(false);
+  const match = window.matchMedia("(min-width: 768px)");
+
   useEffect(() => {
-    window.addEventListener("resize", matchMedia);
-    return () => window.removeEventListener("resize", matchMedia);
+    match.addEventListener("change", handleMediaEvent);
+    handleMediaEvent(match);
+    return () => match.removeEventListener("change", handleMediaEvent);
   });
 
   return <>{isMatchingMedia ? <NavDesktop /> : <NavMobile />}</>;
 
-  function matchMedia() {
-    const match = window.matchMedia("(min-width: 768px)").matches;
-    match ? setIsMatchingMedia(true) : setIsMatchingMedia(false);
+  function handleMediaEvent(mediaEvent) {
+    mediaEvent.matches ? setIsMatchingMedia(true) : setIsMatchingMedia(false);
   }
 }
 
