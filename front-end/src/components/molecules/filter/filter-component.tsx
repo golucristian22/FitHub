b2ad9ae46a21fbbaa9152dd4e9b45be25784d819
G.Cristian <golu.cristian22@gmail.com>
2023-05-24 14:05:43 +0300

    fix(front-end/filter): now the title changes when an option is selected

--- front-end/src/components/molecules/filter/filter-component.tsx
+++ front-end/src/components/molecules/filter/filter-component.tsx
@@ -9,16 +9,17 @@ interface filterProperties {
 
 function Filter(props: filterProperties) {
   const [isOpen, setIsOpen] = useState(false);
+  const [title, setTitle] = useState(props.title);
 
   return (
     <div className={`filter ${isOpen ? "filter--open" : ""}`} onClick={onFilterClick}>
       <div className="filter__header">
-        <h5 className="header__heading">{props.title}</h5>
+        <h5 className="header__heading">{title}</h5>
         <Icon iconName="chevron-down" iconColor="#fff" iconHeight="24px" iconWidth="24px" />
       </div>
       <div className="filter__content">
-        {props.children.map((child) => {
-          return <p className="content__option text-size-lg">{child}</p>
+        {props.children.map((child, index) => {
+          return <p className="content__option text-size-lg" onClick={(e) => onOptionClick(e)} key={index}>{child}</p>
         })}
       </div>
     </div>
@@ -26,6 +27,10 @@ function Filter(props: filterProperties) {
 
   function onFilterClick() {
     setIsOpen(!isOpen);
+  }
+
+  function onOptionClick(e: React.MouseEvent<HTMLParagraphElement, MouseEvent>) {
+    setTitle(e.currentTarget.innerText);
   }
 }
 
