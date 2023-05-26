850db51aec530b027c2b2a75e22818cca980755e
G.Cristian <golu.cristian22@gmail.com>
2023-05-26 11:53:19 +0300

    feat(front-end/save-to-wishlist): created component

---
+++ front-end/src/components/molecules/save-to-wishlist/save-to-wishlist-component.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Icon from "../../atoms/icon/icon-component";
+import "./save-to-wishlist-styles.scss";
+
+function SaveToWishList() {
+  const [isSelected, setIsSelected] = useState(false);
+
+  return (
+    <div
+      className="save-to-wishlist"
+      onClick={() => setIsSelected(!isSelected)}
+    >
+      <Icon
+        iconName={isSelected ? "heart-filled" : "heart"}
+        iconColor="var(--color-accent-secondary"
+        iconHeight="30px"
+        iconWidth="30px"
+      />
+      <p className="text-color-primary text-size-lg">Save to Wishlist</p>
+    </div>
+  );
+}
+
+export default SaveToWishList;
