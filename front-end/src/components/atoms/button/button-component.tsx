6c95df6856304a97facb2780a533d47da14661d6
G.Cristian <golu.cristian22@gmail.com>
2023-05-27 13:41:00 +0300

    feat(frond-end/button): added new property & styles

--- front-end/src/components/atoms/button/button-component.tsx
+++ front-end/src/components/atoms/button/button-component.tsx
@@ -3,22 +3,19 @@ import "./button-styles.scss";
 
 interface ButtonPropertiesInterface {
   variant: "primary" | "secondary";
+  isLarge?: boolean;
   href?: string;
   text: React.ReactNode | string;
 }
 
 function Button(props: ButtonPropertiesInterface) {
   const button = (
-    <div className={`button button--${props.variant}`}>
-      {props.text}
-    </div>
-  )
+    <div className={`button button--${props.variant} ${props.isLarge ? "button--large" : ""}`}>{props.text}</div>
+  );
 
   const buttonAsLink = (
-    <a className={`button button--${props.variant}`} href={props.href}>
-      {props.text}
-    </a>
-  )
+    <a className={`button button--${props.variant} ${ props.isLarge ? "button--large" : ""}`} href={props.href}>{props.text}</a>
+  );
 
   return props.href ? buttonAsLink : button;
 }
